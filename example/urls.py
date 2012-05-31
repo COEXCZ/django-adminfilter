74834f5b19fa2d6b189284fbe1a5a3e1d95d35ad
Tomas Vavrys <tomas.vavrys@coex.cz>
2012-05-31 09:46:16 +0200

    urls change password patch

--- example/urls.py
+++ example/urls.py
@@ -15,6 +15,7 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
+    url(r'^admin/password_change/$',  'django.views.defaults.page_not_found'),
     url(r'^admin/', include(admin.site.urls)),
 )
 
